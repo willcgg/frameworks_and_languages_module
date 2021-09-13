06ee40a99bfe2a67b0486e140efa475def04b272
Allan Callaghan <calaldees@gmail.com>
2021-09-13 15:45:35 +0100

    Cpress commands for signing in and out

--- client_test/cypress/integration/my/freecycle.spec.js
+++ client_test/cypress/integration/my/freecycle.spec.js
@@ -1,7 +1,29 @@
 describe('FreeCycle', () => {
 
+	Cypress.Commands.add('signin', (user_id, password) => {
+		user_id = user_id || `testname${Cypress._.random(0, 1e6)}`;
+		cy.get("#user").contains("signin").click();
+		cy.get('#main input[name="username"]').type(user_id);
+		cy.get('#main input[name="password"]').type(password || 'password');
+		cy.get('#main #action_signin').click();
+
+		cy.get("#user").contains(user_id);
+		cy.get("#user").contains('signout');
+
+		//return user_id;
+	})
+	Cypress.Commands.add('signout', () => {
+		cy.get("#user").contains("signout").should('be.visible').click();
+		cy.get("#user").contains('signin').should('be.visible');
+	})
+
+	Cypress.Commands.add('navigate', (item) => {
+		cy.get("#nav").contains(item).should('be.visible').click();
+	})
+
 	beforeEach(() => {
 		cy.visit('');  // Navigate to Env variable `CYPRESS_BASE_URL`
+		// Local storage is reset per test, so the user should not be logged in
 	});
 
 	it('navigation contents', () => {
@@ -10,7 +32,7 @@ describe('FreeCycle', () => {
 			.should('include.text', 'Items')
 			.should('include.text', 'MyItems')
 		;
-		cy.contains('#nav', 'signin').should('be.visible');
+		cy.get('#nav').contains('signin').should('be.visible');
 	});
 
 	it('logo navigates to welcome page', () => {
@@ -20,5 +42,22 @@ describe('FreeCycle', () => {
 		cy.contains("FreeCycle").click();
 		cy.contains("Welcome").should('be.visible');
 	});
+
+	it('signin and out', () => {
+		cy.signin();
+		cy.signout();
+	});
+
+	it('MyItems redirects to signin if not signed-in', () => {
+		cy.navigate("MyItems");
+		cy.get("#action_signin").should('be.visible');
+	});
+
+	it('MyItems should show is signed in', () => {
+		cy.signin();
+		cy.navigate("MyItems");
+		cy.get("#main").contains('My Page').should('be.visible');
+	});
+
 
 });

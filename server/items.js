6a650298f28d3154ab99320e70023388478ff38d
willcgg <wc104@canterbury.ac.uk>
2021-12-06 17:01:10 +0000

    Added middleware to log incoming requests Tidied up code in server.js Coded items.js javascript object Created a static 'public' file to hold any static files e.g. html, css Attempted fixing dockerfile, could not get working

---
+++ server/items.js
@@ -0,0 +1,15 @@
+
+const items = {
+1: {
+    "id": 0,
+    "user_id": "User Id",
+    "keywords": ["list", "of", "key", "words"],
+    "description": "Item Description",
+    "image": "https://placekitten.com/301/302",
+    "latitude": 0,
+    "longitude": 0,
+    "date_from": "2019-08-24T14:15:22Z",
+    "date_to": "2019-08-24T14:15:22Z"
+    }
+}
+  module.exports = items;
--- server/server.js
+++ server/server.js
@@ -1,33 +1,53 @@
 "use strict";
 
+//app modules
+const express = require('express');
+const { hostname } = require('os');
+const path = require('path');
+const items = require('./items');
+const { time } = require('console');
+
 //constants
 const port = 8000;
-const host = '0.0.0.0';
+const app = express();
 
-//app
-const express = require('express')
-const path = require('path')
+//middleware
+//logs any requests to the console along with the ip address it was from and time it was made
+const logger = (req, res, next) => {
+  var date = new Date();
+  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}` + " request made by: " + 
+  req.headers['x-forwarded-for'] + " at: " + date);
+  next();
+}
 
-const app = express()
+//initializing middleware
+app.use(logger);
 
-//requests
-//home page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-  })
-  .post('/', (req, res) => {
+//making 'public' a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//ROUTES
+//add new item to page
+app.post('/items/', (req, res, user_id, keywords, description, latitude, longitude) => {
+  //checks request isn't empty
+    if(user_id == "" || keywords == "" || description == ""| latitude == 0 || longitude == 0)
+    {
+      res.Send("<h1>ERROR 405 \n Invalid Input</h1>"); 
+      return;
+    }
     
-  })
-  .put('/', (req, res) => {
-
-  })
-  .delete('/', (req, res) => {
+  })//gets items from search
+  .get('/items/', (req, res) => {
 
   })
 
-//about page
-app.get('/about', (req, res) => {
-    res.send('<h1>ABOUT</h1>');
+//getting item by id
+app.get('/items/{itemId}', (req, res) => {
+    
+})
+//delete item by id
+.delete('/items/{itemId}', (req, res) => {
+
 })
 
 app.listen(port, (err) => {

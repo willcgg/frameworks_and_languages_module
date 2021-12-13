0bb423153200bbfd1bdf8da4005409edc375c2f7
willcgg <wc104@canterbury.ac.uk>
2021-12-13 15:11:27 +0000

    Added .dockerignore Amended dockerfile Added extra test items Configured routes in server.js

--- server/items.js
+++ server/items.js
@@ -1,6 +1,6 @@
 
-const items = {
-1: {
+const items = [
+  {
     "id": 0,
     "user_id": "User Id",
     "keywords": ["list", "of", "key", "words"],
@@ -10,6 +10,39 @@ const items = {
     "longitude": 0,
     "date_from": "2019-08-24T14:15:22Z",
     "date_to": "2019-08-24T14:15:22Z"
-    }
-}
+    },
+    {
+      "id": 1,
+      "user_id": "test1",
+      "keywords": ["list", "of", "key", "words", "again"],
+      "description": "Test Item Description",
+      "image": "https://placekitten.com/302/302",
+      "latitude": 1,
+      "longitude": 2,
+      "date_from": "2020-08-24T14:15:22Z",
+      "date_to": "2020-08-24T14:15:22Z"
+    },
+    {
+      "id": 2,
+      "user_id": "User Id",
+      "keywords": ["list", "of", "key", "words"],
+      "description": "Item Description",
+      "image": "https://placekitten.com/301/302",
+      "latitude": 0,
+      "longitude": 0,
+      "date_from": "2019-08-24T14:15:22Z",
+      "date_to": "2019-08-24T14:15:22Z"
+      }
+      {
+        "id": 3,
+        "user_id": "User Id",
+        "keywords": ["list", "of", "key", "words"],
+        "description": "Item Description",
+        "image": "https://placekitten.com/301/302",
+        "latitude": 0,
+        "longitude": 0,
+        "date_from": "2019-08-24T14:15:22Z",
+        "date_to": "2019-08-24T14:15:22Z"
+        }
+  ]
   module.exports = items;
--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@
 const express = require('express');
 const { hostname } = require('os');
 const path = require('path');
-const items = require('./items');
 const { time } = require('console');
+const items = require('./items');
 
 //constants
 const port = 8000;
@@ -15,8 +15,8 @@ const app = express();
 //logs any requests to the console along with the ip address it was from and time it was made
 const logger = (req, res, next) => {
   var date = new Date();
-  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}` + " request made by: " + 
-  req.headers['x-forwarded-for'] + " at: " + date);
+  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}` + "\nRequest made by: " +
+    req.headers['x-forwarded-for'] + "\nAt: " + date);
   next();
 }
 
@@ -26,37 +26,56 @@ app.use(logger);
 //making 'public' a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Item routes: handles every endpoint with /items/
 //ROUTES
-//add new item to page
-app.post('/items/', (req, res, user_id, keywords, description, latitude, longitude) => {
-  //checks request isn't empty
-    if(user_id == "" || keywords == "" || description == ""| latitude == 0 || longitude == 0)
-    {
-      res.Send("<h1>ERROR 405 \n Invalid Input</h1>"); 
+
+app
+  //gets all items
+  .get((req, res) => {
+    res.json(items);
+  })
+app
+  //add new item to page
+  .post('/item/', (req, res, user_id, keywords, description, latitude, longitude) => {
+    if (user_id == "" || keywords == "" || description == "" | latitude == 0 || longitude == 0) {
+      res.status(405).json({ msg: `No item with the id of ${req.params.itemId}` });
+      res.Send("<h1>ERROR 405</h1><p>Invalid Input: Maybe you missed a field</p>\n<p><a href=" / ">Back to homepage</a></p>");
       return;
     }
-    
-  })//gets items from search
-  .get('/items/', (req, res) => {
+  })
+  //gets single item by id
+  .get("/item/:itemId", (req, res) => {
+    //checks items.js has that particular itemId
+    const findItem = items.some(member => member.id === parseInt(req.params.itemId));
+
+    if (findItem) {
+      //item found
+      res.json(items.filter(item => item.id === parseInt(req.params.itemId)));
+    }
+    else {
+      //item not found => return error 
+      res.status(400).json({ msg: `No item with the id of ${req.params.itemId}` });
+      res.send('<h1>ERROR 404</h1>\n<p>Item not found: Maybe the itemId you entered was incorrect.</p>\n<a href="/">Back to homepage</a>');
+    }
+  })
+  //delete single item by id
+  .delete((req, res) => {
 
   })
 
-//getting item by id
-app.get('/items/{itemId}', (req, res) => {
+  //gets items based on search 
+  app
+  .get('/items/', (req, res) =>
+  {
     
-})
-//delete item by id
-.delete('/items/{itemId}', (req, res) => {
-
-})
+  })
 
 app.listen(port, (err) => {
-      //error handling
-    if(err)
-    {
-        console.log("There was a problem with the server: ", err);
-        return;
-    }
-    //no errors app listening
-    console.log(`Example app listening at port 8000...`)
-  })
+  //error handling
+  if (err) {
+    console.log("There was a problem with the server: ", err);
+    return;
+  }
+  //no errors app listening
+  console.log(`Example app listening at port 8000...`)
+})
